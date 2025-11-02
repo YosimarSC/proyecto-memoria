660e5a7182ed24516bca2b184061a2112c5a73ea
yosi <josimarsolano72@gmail.com>
2025-11-02 20:27:13 +0000

    agregue metodos para llenar matrices

--- main.c
+++ main.c
@@ -10,8 +10,28 @@
 
 char memoriaPrincipal[81][6][cantidadLetrasMP];
 char memoriaSecundaria [162][6][cantidadLetrasMS];
-int main (void){
 
+
+void cargarPalabrasMemoriaPrincipal(){
+    
+    for(int i=0;i<81;i++){
+        for(int j=0;j<6;j++){
+            strcpy(memoriaPrincipal[i][j], "");  
+        }
+    }
+}
+
+void cargarPalabrasMemoriaSecundaria(){
+    
+    for(int i=0;i<162;i++){
+        for(int j=0;j<6;j++){
+            strcpy(memoriaSecundaria[i][j], "");
+        }
+    }
+}
+
+
+int main (void){
 
 
 
