555cdf5229b51b09f555d366d0c13005e77ba1be
yosi <josimarsolano72@gmail.com>
2025-11-02 17:34:06 +0000

    inicio del  main

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+// ------variables globales----- 
+
+int cantidadLetrasMP=20;
+int cantidadLetrasMS=20;
+
+char memoriaPrincipal[81][6][cantidadLetrasMP];
+char memoriaSecundaria [162][6][cantidadLetrasMS];
+int main (void){
+
+
+
+
+}
